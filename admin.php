08f59b96f271f15cfd1ed4765d87685cfe5ec24a
HAN ZAW HTET <hanzawhtet317@gmail.com>
2023-03-03 14:21:20 +0630

    admin.php coded

---
+++ admin.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin Dashboard</title>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+</head>
+<body>
+    <nav class="navbar navbar-expand-lg container bg-warning">
+        <h2 class="navbar-brand">Admin Dashboard</h2>
+        <nav class="container navbar-nav">
+            <div class="flex-fill"></div>
+            <span class="nav-item">
+                <a href="#" class="nav-link">User</a>
+            </span>
+            <span class="nav-item">
+                <a href="#" class="nav-link">Logout</a>
+            </span>
+        </nav>
+    </nav>
+    <main class="container">
+        <table class="table">
+            <thead>
+                <th>ID</th>
+                <th>Name</th>
+                <th>Email</th>
+                <th>Controls</th>
+            </thead>
+        </table>
+    </main>
+</body>
+</html>
